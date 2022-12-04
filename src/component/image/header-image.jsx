91f1baf7595abe64ec42e7e87fd2d8b6f6d75ce4
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2022-12-04 18:15:25 +0100

    complete all Todos on files

--- src/component/image/header-image.jsx
+++ src/component/image/header-image.jsx
@@ -1,15 +1,9 @@
-import React from 'react';
-// import taxpayImg from '../images/tax-payment-1.jpg';
-import nodbey from '../../images/NODBEYLOGO.jpg';
+import React from "react";
+import nodbey from "../../images/NODBEYLOGO.jpg";
 
 function Image() {
-   // const img = taxpayImg;
-   const img = nodbey;  
-   return(
-        <img className='img' src={img} alt='Header tax man'></img>
-   )
-    
+  const img = nodbey;
+  return <img className="img" src={img} alt="Header tax man"></img>;
 }
 
-export default Image
-
+export default Image;
