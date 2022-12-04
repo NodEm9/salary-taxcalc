657a571e7361f9ab610388ffd34524eee95f3f67
NodEm9 <42060362+NodEm9@users.noreply.github.com>
2022-12-04 18:14:35 +0100

    complete all Todos on files

--- src/component/tax.jsx
+++ src/component/tax.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import TaxInput from "./tax-calc";
-import { ThemeContext } from "../../ThemeContext/theme-context";
-    
+
 class Tax extends React.Component {
   constructor(props) {
     super(props);
     this.inputElement = React.createRef();
   }
-  
+
   render() {
-       return(
-       <TaxInput inputRef={this.inputElement} />
-    )
+    return <TaxInput inputRef={this.inputElement} />;
   }
-}  
+}
 
 export default Tax;
